5094ffd634b52b88d690a6537badeee37bc12fd7
govind <govind.g.coder@gmail.com>
2026-01-15 12:31:48 +0530

    removed the 👍 emojies for the tasks

--- src/SumOfNaturalNumbers.java
+++ src/SumOfNaturalNumbers.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class SumOfNaturalNumbers {
 
-    // TODO: Create the method public static int NumberSum(int n) 👍
+    // TODO: Create the method public static int NumberSum(int n)
     // It should calculate and return the sum of first n natural numbers 
 
 
         // Inside main method
-        // TODO: Read N using Scanner 👍
-        // TODO: Call NumberSum(N) 👍
-        // TODO: Print the result 👍
+        // TODO: Read N using Scanner
+        // TODO: Call NumberSum(N)
+        // TODO: Print the result
     
     public static int NumberSum(int n){
         int sum = 0;
